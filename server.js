0d25094bb376e2e4252a471b715ba31be8886f9e
TravisGuillory <travisguillory70@gmail.com>
2020-04-01 19:59:54 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,6 +4,37 @@ var path = require("path");
 var app = express();
 var PORT = 8000;
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Create arrays to hold basic data
+
+let tables = []
+
+
+
+
+
+// Routes for getting and posting data
+
+
+
+
+
+
+// routres for displaying the html pages
+
+
+
+
+
+
+
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
+
